353d41a376aca9e42683e522004b4f89788d3448
BenildoMorais <benildomorais360@gmail.com>
2022-09-26 19:21:19 +0200

    Exercitando TPC2 Aula 09092022

--- src/Aula09092022/TPC2.java
+++ src/Aula09092022/TPC2.java
@@ -10,26 +10,35 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 public class TPC2 {
 
 	private JFrame fr;
-	private JPanel PE, PC, PCUp, PCUp1, PCUp2, PCUp3, PCUp4, PCUp5, PCDow, PCDow1, PCDow2;
-	private JButton bt1, bt2, bt3, bt4;
+	private JPanel PE, PC, PCa, PCb, PCUp, PCUp1, PCUp2, PCUp3, PCUp4, PCUp5, PCDow, PCDow1, PCDow1a, PCDow1b, PCDow1c, PCDow2;
+	private JButton bt1, bt2, bt3, bt4, btl, btr;
 	private JLabel tx1, tx2, tx3, tx3a, tx4, tx5;
 	private JTextField lb1, lb2, lb3;
 	private JRadioButton rdf, rdm;
 	private ButtonGroup gpG;
 	private JCheckBox ch1, ch2, ch3;
 	private JComboBox cb;
+	private JList lista;
+	private JTextArea txa, txb;
+	private JScrollPane sc1, sc2;
 	
 	public TPC2() {
 		fr = new JFrame();
 		PE = new JPanel();
 		PC = new JPanel();
+		PCa = new JPanel();
+		PCb = new JPanel();
 		PCUp = new JPanel();
 		PCUp1 = new JPanel();
 		PCUp2 = new JPanel();
@@ -38,7 +47,22 @@ public class TPC2 {
 		PCUp5 = new JPanel();
 		PCDow = new JPanel();
 		PCDow1 = new JPanel();
+		PCDow1a = new JPanel();
+		PCDow1b = new JPanel();
+		PCDow1c = new JPanel();
 		PCDow2 = new JPanel();
+		
+		txa = new JTextArea(5,20);
+		txb = new JTextArea(6,20);
+		
+		String [] objectos = {"Redes","Inernet","Compiladores","Segurancs","BD"};
+		lista = new JList(objectos);
+		sc1 = new JScrollPane(lista);
+		sc2 = new JScrollPane(txb);
+		//sc1.add(lista);
+		
+		btl = new JButton("Esquerda");
+		btr = new JButton("Direita");
 		
 		ch1 = new JCheckBox("Curso 1");
 		ch2 = new JCheckBox("Curso 2");
@@ -47,22 +71,26 @@ public class TPC2 {
 		gpG = new ButtonGroup();
 		rdf = new JRadioButton ("Femenino");
 		rdm = new JRadioButton("Masculino");
+		gpG.add(rdf);
+		gpG.add(rdm);
+		
 		
 		tx1 = new JLabel("Nome: ");
 		lb1 = new JTextField(20);
+		
 		
 		tx2 = new JLabel("Endereco: ");
 		lb2 = new JTextField(20);
 		
 		tx3 = new JLabel("Cidade: ");
 		lb3 = new JTextField(20);
-		tx3a = new JLabel("Estado");
+		tx3a = new JLabel("Estado: ");
 		
 		tx4 = new JLabel("Sexo: ");
 		tx5 = new JLabel("Opcoes: ");
-		
+				
 		fr.setSize(450,500);
-		fr.setTitle("TelaRegistro");
+		fr.setTitle("TelaRegistro TPC");
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fr.setLocationRelativeTo(null);
 		
@@ -74,38 +102,71 @@ public class TPC2 {
 		fr.setLayout(new BorderLayout());
 		
 		
-		PE.setLayout(new GridLayout(10,1,20,10));
+		PE.setLayout(new GridLayout(10,1,2,2));
 		PE.add(bt1);
 		PE.add(bt2);
 		PE.add(bt3);
 		PE.add(bt4);
 		
+		
 		PC.setLayout(new GridLayout(2,1));
-		PCUp.setLayout(new BorderLayout());
-		PCUp1.setLayout(new GridLayout(5,1));
+		PCa.setLayout(new GridLayout(5,1));
+		
+		PCUp1.setLayout(new FlowLayout());
 		PCUp1.add(tx1);
-		PCUp1.add(tx2);
-		PCUp1.add(tx3);
-		PCUp1.add(tx4);
-		PCUp1.add(tx5);
-		PCUp.add("West",PCUp1);
+		PCUp1.add(lb1);
+		PCa.add(PCUp1);
 		
-		PCUp2.setLayout(new GridLayout(5,1));
-		PCUp2.add(lb1);
+		PCUp2.setLayout(new FlowLayout());
+		PCUp2.add(tx2);
 		PCUp2.add(lb2);
+		PCa.add(PCUp2);
+		
 		PCUp3.setLayout(new FlowLayout());
+		PCUp3.add(tx3);
 		PCUp3.add(lb3);
+		PCUp3.add(tx3a);
+		PCa.add(PCUp3);
+
+		PCUp4.setLayout(new FlowLayout());
+		PCUp4.add(tx4);
+		PCUp4.add(rdm);
+		PCUp4.add(rdf);
+		PCa.add(PCUp4);
+		
+		PCUp5.setLayout(new FlowLayout());
+		PCUp5.add(tx5);
+		PCUp5.add(ch1);
+		PCUp5.add(ch2);
+		PCUp5.add(ch3);
+		PCa.add(PCUp5);
+		
+			PC.add(PCa);
+		
+		PCb.setLayout(new GridLayout(2,1));
+		
+		PCDow1.setLayout(new GridLayout(1,3));
+		PCDow1.add(sc1);
+		
+		PCDow1a.setLayout(new GridLayout(4,1));
+		PCDow1a.add(btl);
+		PCDow1a.add(btr);
+		PCDow1a.add(new JLabel());
+		PCDow1a.add(new JLabel());
+		PCDow1.add(PCDow1a);
+		PCDow1.add(txa);
+		
+		PCDow1.setBorder(new TitledBorder("Interesses"));
 		
 		
+		PCb.add(PCDow1);
 		
-		PCUp.add("Center", PCUp2);
+		PCDow2.setLayout(new FlowLayout());
+		PCDow2.add(sc2);
+		PCb.add(sc2);
 		
-		PC.add(PCUp);
-		
-		
-		PCDow.setLayout(new GridLayout(2,1));
-		PC.add(PCDow);
-		
+		PC.add(PCa);
+		PC.add(PCb);
 		
 		fr.add("Center",PC);
 		fr.add("East",PE);
--- src/Aula09092022/Teste.java
+++ src/Aula09092022/Teste.java
@@ -2,9 +2,9 @@ package Aula09092022;
 
 public class Teste {
 	public static void main(String[] args) {
-		new TelaRegistro();
+		//new TelaRegistro();
 		//new TPC();
-		//new TPC2();
+		new TPC2();
 	}
 
 }
